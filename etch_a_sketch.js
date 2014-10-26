60a197e43c93c367dbe6a6a4682b45805fb8e5e4
Galen Scovell <galen.scovell@gmail.com>
2014-10-25 19:49:15 -0700

    Finish Normal Mode, Gradual Mode, Trail Mode
    Color mode remains, as well as aesthetic pass.

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,33 +1,74 @@
 
 $(document).ready(function() {
-	console.log("Ready!");
-	var columns = 10;
-	var mode = 'normal';
+	var columns = 0;
+	var mode = '';
 
 
-	columnNumber();
-
-
-	switch (mode) {
-		case 'normal':
-		normalMode();
-		break;
-	}
-
+	$('.button').click(function () {
+		mode = $(this).attr('id');
+		switch (mode) {
+			case 'color':
+				columnNumber();
+				colorMode();
+				break;
+			case 'trail':
+				columnNumber();
+				trailMode();
+				break;
+			case 'gradual':
+				columnNumber();
+				gradualMode();
+				break;
+			case 'normal':
+				columnNumber();
+				normalMode();
+				break;
+		}	
+	})
 });
 
 
+
+// Mode functions
 function normalMode() {
 	$('div.column').mouseenter(function() {
-		$(this).css('opacity', 0.6);
+		$(this).css('opacity', 1.0);
+	});
+}
+
+function colorMode() {
+	$('div.column').mouseenter(function() {
+		$(this).css('opacity', 0.0);
+	});
+}
+
+function trailMode() {
+	$('div.column').mouseenter(function() {
+		$(this).fadeTo(10, 1);
+		$(this).mouseleave(function() {
+			$(this).fadeTo(300, 0.3);
+		});
+	});
+}
+
+function gradualMode() {
+	$('div.column').mouseenter(function() {
+		var block_opacity = $(this).css("opacity");
+		if (block_opacity < 1) {
+			$(this).css("opacity", block_opacity * 2);
+		}
 	});
 }
 
 
+
+// Board creation functions
 function gridCreation(size) {
 
+	$('.row').remove();
+
 	for(var i = 0; i < size; i++) {
-		$('#container').append('<div class="row"</div>');
+		$('#container').append('<div class="row"></div>');
 	}
 
 	for(var n = 0; n < size; n++) {
@@ -36,11 +77,11 @@ function gridCreation(size) {
 }
 
 function columnNumber() {
-	var rows = prompt("How many rows? (1-10)");
+	var rows = prompt("How many rows/columns? (1-10)");
 
 	while (rows < 1 || rows > 10) {
 		alert("Outside of range.");
-		rows = prompt("How many rows? (1-20)");
+		rows = prompt("How many rows/columns? (1-10)");
 	}
 
 	gridCreation(rows);
