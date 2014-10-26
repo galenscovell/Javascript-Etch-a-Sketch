b743cdedeff4eddcded75d689ff1cd2db3063e34
Galen Scovell <galen.scovell@gmail.com>
2014-10-26 16:07:50 -0700

    Fix Indentation Issues

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,35 +1,35 @@
 
 // Ready document functions
 $(document).ready(function() {
-	var columns = 0;
-	var mode = '';
+    var columns = 0;
+    var mode = '';
 
     // Mode selection
-	$('.button').click(function () {
-		mode = $(this).attr('id');
-		switch (mode) {
-			case 'color':
-				columnNumber();
-				colorMode();
-				break;
-			case 'trail':
-				columnNumber();
-				trailMode();
-				break;
-			case 'gradual':
-				columnNumber();
-				gradualMode();
-				break;
-			case 'normal':
-				columnNumber();
-				normalMode();
-				break;
+    $('.button').click(function () {
+        mode = $(this).attr('id');
+        switch (mode) {
+            case 'color':
+                columnNumber();
+                colorMode();
+                break;
+            case 'trail':
+                columnNumber();
+                trailMode();
+                break;
+            case 'gradual':
+                columnNumber();
+                gradualMode();
+                break;
+            case 'normal':
+                columnNumber();
+                normalMode();
+                break;
             case 'toggle':
                 columnNumber();
                 toggleMode();
                 break;
-		}	
-	})
+        }    
+    })
 });
 
 
@@ -37,39 +37,39 @@ $(document).ready(function() {
 
 // Mode functions
 function normalMode() {
-	$('div.column').mouseenter(function() {
-		$(this).css('opacity', 1.0);
-	});
+    $('div.column').mouseenter(function() {
+        $(this).css('opacity', 1.0);
+    });
 }
 
 function colorMode() {
-	$('div.column').mouseenter(function() {
-		
+    $('div.column').mouseenter(function() {
+        
         var red = Math.floor(Math.random() * 256);
         var green = Math.floor(Math.random() * 256);
         var blue = Math.floor(Math.random() * 256);
         
         $(this).css('background', "rgb(" + red +", " + green +", " + blue +")");
         $(this).css("opacity", 0.8);
-	});
+    });
 }
 
 function trailMode() {
-	$('div.column').mouseenter(function() {
-		$(this).fadeTo(0, 1);
-		$(this).mouseleave(function() {
-			$(this).fadeTo(600, 0.3);
-		});
-	});
+    $('div.column').mouseenter(function() {
+        $(this).fadeTo(0, 1);
+        $(this).mouseleave(function() {
+            $(this).fadeTo(600, 0.3);
+        });
+    });
 }
 
 function gradualMode() {
-	$('div.column').mouseenter(function() {
-		var block_opacity = $(this).css("opacity");
-		if (block_opacity < 1) {
-			$(this).css("opacity", block_opacity * 2);
-		}
-	});
+    $('div.column').mouseenter(function() {
+        var block_opacity = $(this).css("opacity");
+        if (block_opacity < 1) {
+            $(this).css("opacity", block_opacity * 2);
+        }
+    });
 }
 
 function toggleMode() {
@@ -91,24 +91,24 @@ function toggleMode() {
 function gridCreation(size) {
 
     $("#container").text("");
-	$('.row').remove();
+    $('.row').remove();
 
-	for(var i = 0; i < size; i++) {
-		$('#container').append('<div class="row"></div>');
-	}
+    for(var i = 0; i < size; i++) {
+        $('#container').append('<div class="row"></div>');
+    }
 
-	for(var n = 0; n < size; n++) {
-		$('.row').append('<div class="column"></div>');
-	}
+    for(var n = 0; n < size; n++) {
+        $('.row').append('<div class="column"></div>');
+    }
 }
 
 function columnNumber() {
-	var rows = prompt("How many rows/columns? (1-19)");
+    var rows = prompt("How many rows/columns? (1-19)");
 
-	while (rows < 1 || rows > 19) {
-		alert("Outside of range.");
-		rows = prompt("How many rows/columns? (1-19)");
-	}
+    while (rows < 1 || rows > 19) {
+        alert("Outside of range.");
+        rows = prompt("How many rows/columns? (1-19)");
+    }
 
-	gridCreation(rows);
+    gridCreation(rows);
 }
