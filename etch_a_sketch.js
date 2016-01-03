18b6cc3f9fc21d264e7c705f9407f5800edbdfad
Galen <galen.scovell@gmail.com>
2016-01-02 16:06:40 -0800

    Cleanup for placement on portfolio

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,61 +1,29 @@
+/**
+ * Etch-a-Sketch dynamic controls
+ * ===============================
+ *
+ * @author GalenS <galen.scovell@gmail.com>
+ */
 
-// Ready document functions
-$(document).ready(function() {
-    var columns = 0;
-    var mode = '';
-
-    // Mode selection
-    $('.button').click(function () {
-        mode = $(this).attr('id');
-        switch (mode) {
-            case 'color':
-                columnNumber();
-                colorMode();
-                break;
-            case 'trail':
-                columnNumber();
-                trailMode();
-                break;
-            case 'gradual':
-                columnNumber();
-                gradualMode();
-                break;
-            case 'normal':
-                columnNumber();
-                normalMode();
-                break;
-            case 'toggle':
-                columnNumber();
-                toggleMode();
-                break;
-        }    
-    })
-});
-
-
-
-
-// Mode functions
 function normalMode() {
-    $('div.column').mouseenter(function() {
-        $(this).css('opacity', 1.0);
+    $("div.column").mouseenter(function() {
+        $(this).css("opacity", 1.0);
     });
 }
 
 function colorMode() {
-    $('div.column').mouseenter(function() {
-        
+    $("div.column").mouseenter(function() {
         var red = Math.floor(Math.random() * 256);
         var green = Math.floor(Math.random() * 256);
         var blue = Math.floor(Math.random() * 256);
         
-        $(this).css('background', "rgb(" + red +", " + green +", " + blue +")");
+        $(this).css("background", "rgb(" + red +", " + green +", " + blue +")");
         $(this).css("opacity", 0.8);
     });
 }
 
 function trailMode() {
-    $('div.column').mouseenter(function() {
+    $("div.column").mouseenter(function() {
         $(this).fadeTo(0, 1);
         $(this).mouseleave(function() {
             $(this).fadeTo(600, 0.3);
@@ -64,7 +32,7 @@ function trailMode() {
 }
 
 function gradualMode() {
-    $('div.column').mouseenter(function() {
+    $("div.column").mouseenter(function() {
         var block_opacity = $(this).css("opacity");
         if (block_opacity < 1) {
             $(this).css("opacity", block_opacity * 2);
@@ -73,7 +41,7 @@ function gradualMode() {
 }
 
 function toggleMode() {
-    $('div.column').mouseenter(function() {
+    $("div.column").mouseenter(function() {
         var current = $(this).css("opacity");
         if (current == 1) {
             $(this).css("opacity", 0.3);
@@ -84,31 +52,38 @@ function toggleMode() {
     });
 }
 
-
-
-
-// Board creation functions
-function gridCreation(size) {
-
+function constructGrid() {
     $("#container").text("");
-    $('.row').remove();
+    $(".row").remove();
 
-    for(var i = 0; i < size; i++) {
-        $('#container').append('<div class="row"></div>');
+    for(var y = 0; y < 20; y++) {
+        $("#container").append("<div class='row'></div>");
     }
 
-    for(var n = 0; n < size; n++) {
-        $('.row').append('<div class="column"></div>');
+    for(var x = 0; x < 22; x++) {
+        $(".row").append("<div class='column'></div>");
     }
 }
 
-function columnNumber() {
-    var rows = prompt("How many rows/columns? (1-19)");
+$(document).ready(function() {
+    constructGrid();
+    normalMode();
 
-    while (rows < 1 || rows > 19) {
-        alert("Outside of range.");
-        rows = prompt("How many rows/columns? (1-19)");
-    }
+    // Mode selection
+    $(".button").click(function () {
+        constructGrid();
+        mode = $(this).attr("id");
 
-    gridCreation(rows);
-}
+        if (mode === "color") {
+            colorMode();
+        } else if (mode === "trail") {
+            trailMode();
+        } else if (mode === "gradual") {
+            gradualMode();
+        } else if (mode === "toggle") {
+            toggleMode();
+        } else {
+            normalMode();
+        }
+    })
+});
